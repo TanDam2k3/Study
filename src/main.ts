c66ef72c965a9595fb1e37240a2def27f834e455
dam2k3 <tandam9902@gmail.com>
2025-01-05 12:34:50 +0700

    .env set up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  dotenv.config();
   await app.listen(process.env.HTTP_PORT);
 }
 bootstrap();
